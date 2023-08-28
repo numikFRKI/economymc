4d213e18cf19ca6d35b2c688d7aa65cc16adf920
Utikeles <142643953+Utikeles@users.noreply.github.com>
2023-08-28 09:47:00 -0400

    Update BalTopCommand.java
    added imports and started baltop

--- src/com/economymc/BalTopCommand.java
+++ src/com/economymc/BalTopCommand.java
@@ -1,1 +1,15 @@
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class BaltopCommand implements CommandExecutor {
+
+    private EconomyManager economyManager;
+
+    public BalanceCommand(EconomyManager economyManager) {
+        this.economyManager = economyManager;
+    }
+
+
 
